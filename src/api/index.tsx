efd87c975ac8aebaa444aa4f7dc5a82a8fe59624
André Rato <arato@ubiwhere.com>
2025-04-10 22:56:15 +0100

    fix: file url

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Papa from 'papaparse';
 import { MatchI } from './models';
 
-const fileUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4F_b_bTV96TxtijYvhWHPjBIVhbhN6idOJRou50noE7O60fS4PNM6VPp0HE0c1g/pub?gid=0&single=true&output=csv';
+const fileUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4F_b_bTV96TxtijYvhWHPjBIVhbhN6idOJRou50noE7O60fS4PNM6VPp0HE0c1g/pub?gid=388600914&single=true&output=csv';
 
 export async function fetchCSVData(): Promise<MatchI[]> {
     const response = await axios.get(fileUrl);
@@ -10,6 +10,7 @@ export async function fetchCSVData(): Promise<MatchI[]> {
     return new Promise((resolve) => {
         Papa.parse(response.data, {
             complete: (result: { data: MatchI[] }) => {
+                console.log(result.data)
                 resolve(result.data);
             },
             header: true,
